f8eb6c9546946bb75cf4a5b504f0d4fe2104a315
Gjellerhjort <mads.gjellerod@gmail.com>
2021-10-26 17:48:57 +0200

    f
    config

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ import discord
 import asyncio
 import datetime
 
-TOKEN_AUTH = "MzU4MTk3NjA0OTk3MjY3NDU4.YVyOPg.BhbQHN9uiyrLUiaycZSD8gmjWdY" # Retrieved from browser local storage
-
+#TOKEN_AUTH = "MzU4MTk3NjA0OTk3MjY3NDU4.YVyOPg.BhbQHN9uiyrLUiaycZSD8gmjWdY" # Retrieved from browser local storage
 client = discord.Client()
+
 filename = "config"
 contents = open(filename).read()
 config = eval(contents)
 TOKEN_AUTH = config['Token_AUTH']
+Channel_ID = config['Channel_ID']
 
 @client.event
 async def on_ready():
     print("ready")
-    channel = client.get_channel(902218110113828878)
-    message = await channel.fetch_message(channel.last_message_id)
+    channel = client.get_channel(int(Channel_ID))
     await channel.send('Bot Active')
     print(client.users)
-    print(message)
 
-
-client.run(TOKEN_AUTH, bot=False)
+if __name__ == '__main__':
+    client.run(TOKEN_AUTH, bot=False)
