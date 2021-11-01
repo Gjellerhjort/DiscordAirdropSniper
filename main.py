0ed0d9f144440976b0ee04db83a9302da7abc6fd
Gjellerhjort <mads.gjellerod@gmail.com>
2021-11-01 20:13:20 +0100

    blacklisting
    added blacklisting of channels

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from discord.message import Message
 import requests
 import json
 
+
+
 client = discord.Client()
 reaction = '\N{party popper}'
-
+AirdropsJoined = 0
 airdropStatus = "false"
 filename = "config"
 contents = open(filename).read()
@@ -20,6 +22,7 @@ TOKEN_AUTH = config['Token_AUTH']
 Channel_ID = config['Channel_ID']
 
 def retrieve_messages(channelid, messageid):
+    bannedchannels = [846864477642031174, 889908778395385936, 846854228386250795, 884115738741325905, 900880235086626846, 847894288322265110, 903483119489933373]
     messageDetailsDict = {}
     headers = {
         'authorization' : TOKEN_AUTH
@@ -27,7 +30,7 @@ def retrieve_messages(channelid, messageid):
     r = requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages', headers=headers)
     jsonn = json.loads(r.text)
     for value in jsonn:
-        if value['id'] == messageid:
+        if value['id'] == messageid and int(channelid) not in bannedchannels:
             messageDetails = value['embeds']
             if len(messageDetails) != 0:
                 messageDetailsDict = dict(messageDetails[-1])
@@ -37,7 +40,7 @@ def retrieve_messages(channelid, messageid):
                             airdropStatus = True
                             return airdropStatus
                 except:
-                    print('no title')
+                    continue
 
 @client.event
 async def on_ready():
@@ -53,7 +56,7 @@ async def on_message(message):
         MessageChannelId = str(message.channel.id)
         airdropStatus = retrieve_messages(MessageChannelId, Messageid)
         if airdropStatus == True:
-            sleep(1)
+            sleep(2)
             print("joined new airdrop in " + message.channel.name)
             await message.add_reaction(reaction)
             airdropStatus = False
